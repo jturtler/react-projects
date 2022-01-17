10c570087a73ff8c4175802cffa4ca9a5b22212d
James Chang <jamesbchang@gmail.com>
2022-01-17 14:40:29 +0900

    update

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -2,9 +2,37 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { useGlobalContext } from './context';
 
-const Sidebar = () => {
-  return <h2>sidebar</h2>
+const Sidebar = () => 
+{
+  const {isSidebarOpen, closeSidebar} = useGlobalContext();
+
+  return <aside className={`sidebar ${ (isSidebarOpen) ? 'show-sidebar': '' }`}>
+    <div className='sidebar-header'>
+      <img src={logo} className='logo' alt='coding addict' />
+      <button className='close-btn' onClick={closeSidebar} >
+        <FaTimes />
+      </button>
+    </div>
+    <ul className='links'>
+      {links.map( link => {
+        return <li key={link.id}>
+          <a href={link.url}>
+            {link.icon}
+            {link.text}
+          </a>
+        </li>
+      })}
+    </ul>
+    <ul className='social-icons'>
+      {social.map( link => {
+        return <li key={link.id}>
+          <a href={link.url}>{link.icon}</a>
+        </li>
+      })}
+    </ul>
+  </aside>
 }
 
 export default Sidebar
--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,22 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => { setIsSidebarOpen(true); };
+    const closeSidebar = () => { setIsSidebarOpen(false); };
+    const openModal = () => { setIsModalOpen(true); };
+    const closeModal = () => { setIsModalOpen(false); };
+
+    return <AppContext.Provider value={{isSidebarOpen, isModalOpen, openSidebar, closeSidebar, openModal, closeModal }}>{children}</AppContext.Provider>
+};
+
+// Custom Hook - for not having many imports, but simply this..
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
